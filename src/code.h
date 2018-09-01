ce1d81b618122a87e1bcac5f83ab48e472156652
kishima <kishima7@gmail.com>
2018-09-01 20:11:47 +0900

    add Arduino symbol

--- src/code.h
+++ src/code.h
@@ -3,11 +3,11 @@ const unsigned char mmruby_code_irep_000[] PROGMEM= {
 0x01,0x0A,0x10,0x02,0x00,0x80,0x00,0x48,0x01,0x00,0x00,0xC0,0x00,0x80,0x00,0x46,
 0x00,0x80,0x00,0x06,0x01,0x00,0x00,0x3D,0x00,0x80,0x40,0xA0,0x00,0x80,0x00,0x06,
 0x01,0x00,0x00,0xBD,0x00,0x80,0x00,0xA0,0x00,0x00,0x00,0x4A,0x00,0x00,0x04,0x54,
-0x65,0x73,0x74,0x00,0x00,0x06,0x48,0x65,0x6C,0x6C,0x6F,0x21,0x1F,0x0C,0x01,
+0x65,0x73,0x74,0x00,0x00,0x06,0x48,0x65,0x6C,0x6C,0x6F,0x21,0x20,0x0D,0x01,
 };
 const unsigned char mmruby_code_irep_001[] PROGMEM= {
 0x00,0x05,0x00,0x01,0x02,0x00,0x00,0x26,0x01,0x80,0x00,0x06,0x02,0x00,0x40,0x01,
-0x01,0x80,0x00,0xA0,0x01,0x80,0x00,0x29,0x0C,
+0x01,0x80,0x00,0xA0,0x01,0x80,0x00,0x29,0x0D,
 };
 const unsigned char* const mmruby_code_irep_table[] PROGMEM = {
   mmruby_code_irep_000,
@@ -19,7 +19,7 @@ const unsigned char mmruby_code_proc_table_size PROGMEM = 7;
 
 
 const uint8_t mmruby_code_proc_table_Object[] PROGMEM = {
-  12,1,
+  13,1,
   0
 };
 
@@ -48,9 +48,9 @@ const uint8_t mmruby_code_proc_table_Fixnum[] PROGMEM = {
 };
 
 const uint8_t mmruby_code_proc_table_String[] PROGMEM = {
-  13,2,
-  14,3,
-  15,4,
+  14,2,
+  15,3,
+  16,4,
   0
 };
 
@@ -63,9 +63,9 @@ const uint8_t mmruby_code_proc_table_Range[] PROGMEM = {
 };
 
 const uint8_t mmruby_code_proc_table_Arduino[] PROGMEM = {
-  32,5,
-  33,6,
-  34,7,
+  33,5,
+  34,6,
+  35,7,
   0
 };
 
@@ -86,7 +86,7 @@ inline mrb_func_t find_c_funcs_by_no(short no){
 
 
 /* Symbol table */
-const unsigned char mmruby_code_symbol_table_size PROGMEM = 35;
+const unsigned char mmruby_code_symbol_table_size PROGMEM = 36;
 const char mmruby_code_symbol_ref00[] PROGMEM ="";
 const char mmruby_code_symbol_ref01[] PROGMEM ="Object";
 const char mmruby_code_symbol_ref02[] PROGMEM ="String";
@@ -99,29 +99,30 @@ const char mmruby_code_symbol_ref08[] PROGMEM ="Symbol";
 const char mmruby_code_symbol_ref09[] PROGMEM ="Array";
 const char mmruby_code_symbol_ref0a[] PROGMEM ="Range";
 const char mmruby_code_symbol_ref0b[] PROGMEM ="Hash";
-const char mmruby_code_symbol_ref0c[] PROGMEM ="puts";
-const char mmruby_code_symbol_ref0d[] PROGMEM ="+";
-const char mmruby_code_symbol_ref0e[] PROGMEM ="===";
-const char mmruby_code_symbol_ref0f[] PROGMEM ="size";
-const char mmruby_code_symbol_ref10[] PROGMEM ="length";
-const char mmruby_code_symbol_ref11[] PROGMEM ="to_i";
-const char mmruby_code_symbol_ref12[] PROGMEM ="to_s";
-const char mmruby_code_symbol_ref13[] PROGMEM ="<<";
-const char mmruby_code_symbol_ref14[] PROGMEM ="[]";
-const char mmruby_code_symbol_ref15[] PROGMEM ="[]=";
-const char mmruby_code_symbol_ref16[] PROGMEM ="chomp";
-const char mmruby_code_symbol_ref17[] PROGMEM ="chomp!";
-const char mmruby_code_symbol_ref18[] PROGMEM ="dup";
-const char mmruby_code_symbol_ref19[] PROGMEM ="index";
-const char mmruby_code_symbol_ref1a[] PROGMEM ="ord";
-const char mmruby_code_symbol_ref1b[] PROGMEM ="to_sym";
-const char mmruby_code_symbol_ref1c[] PROGMEM ="intern";
-const char mmruby_code_symbol_ref1d[] PROGMEM ="sprintf";
-const char mmruby_code_symbol_ref1e[] PROGMEM ="()";
-const char mmruby_code_symbol_ref1f[] PROGMEM ="method";
-const char mmruby_code_symbol_ref20[] PROGMEM ="pin_mode";
-const char mmruby_code_symbol_ref21[] PROGMEM ="digital_write";
-const char mmruby_code_symbol_ref22[] PROGMEM ="digital_read";
+const char mmruby_code_symbol_ref0c[] PROGMEM ="Arduino";
+const char mmruby_code_symbol_ref0d[] PROGMEM ="puts";
+const char mmruby_code_symbol_ref0e[] PROGMEM ="+";
+const char mmruby_code_symbol_ref0f[] PROGMEM ="===";
+const char mmruby_code_symbol_ref10[] PROGMEM ="size";
+const char mmruby_code_symbol_ref11[] PROGMEM ="length";
+const char mmruby_code_symbol_ref12[] PROGMEM ="to_i";
+const char mmruby_code_symbol_ref13[] PROGMEM ="to_s";
+const char mmruby_code_symbol_ref14[] PROGMEM ="<<";
+const char mmruby_code_symbol_ref15[] PROGMEM ="[]";
+const char mmruby_code_symbol_ref16[] PROGMEM ="[]=";
+const char mmruby_code_symbol_ref17[] PROGMEM ="chomp";
+const char mmruby_code_symbol_ref18[] PROGMEM ="chomp!";
+const char mmruby_code_symbol_ref19[] PROGMEM ="dup";
+const char mmruby_code_symbol_ref1a[] PROGMEM ="index";
+const char mmruby_code_symbol_ref1b[] PROGMEM ="ord";
+const char mmruby_code_symbol_ref1c[] PROGMEM ="to_sym";
+const char mmruby_code_symbol_ref1d[] PROGMEM ="intern";
+const char mmruby_code_symbol_ref1e[] PROGMEM ="sprintf";
+const char mmruby_code_symbol_ref1f[] PROGMEM ="()";
+const char mmruby_code_symbol_ref20[] PROGMEM ="method";
+const char mmruby_code_symbol_ref21[] PROGMEM ="pin_mode";
+const char mmruby_code_symbol_ref22[] PROGMEM ="digital_write";
+const char mmruby_code_symbol_ref23[] PROGMEM ="digital_read";
 
 const char* const mmruby_code_symbol_table[] PROGMEM = {
   mmruby_code_symbol_ref00,
@@ -159,6 +160,7 @@ const char* const mmruby_code_symbol_table[] PROGMEM = {
   mmruby_code_symbol_ref20,
   mmruby_code_symbol_ref21,
   mmruby_code_symbol_ref22,
+  mmruby_code_symbol_ref23,
 };
 
 /* Symbol IDs */
@@ -173,22 +175,23 @@ const char* const mmruby_code_symbol_table[] PROGMEM = {
 #define MRBC_SSYM_Array (9) // Array
 #define MRBC_SSYM_Range (10) // Range
 #define MRBC_SSYM_Hash (11) // Hash
-#define MRBC_SSYM_puts (12) // puts
-#define MRBC_SSYM_ESC_01 (13) // +
-#define MRBC_SSYM_ESC_02 (14) // ===
-#define MRBC_SSYM_size (15) // size
-#define MRBC_SSYM_length (16) // length
-#define MRBC_SSYM_to_i (17) // to_i
-#define MRBC_SSYM_to_s (18) // to_s
-#define MRBC_SSYM_ESC_03 (19) // <<
-#define MRBC_SSYM_ESC_04 (20) // []
-#define MRBC_SSYM_ESC_05 (21) // []=
-#define MRBC_SSYM_chomp (22) // chomp
-#define MRBC_SSYM_ESC_06 (23) // chomp!
-#define MRBC_SSYM_dup (24) // dup
-#define MRBC_SSYM_index (25) // index
-#define MRBC_SSYM_ord (26) // ord
-#define MRBC_SSYM_to_sym (27) // to_sym
-#define MRBC_SSYM_intern (28) // intern
-#define MRBC_SSYM_sprintf (29) // sprintf
-#define MRBC_SSYM_ESC_LAMBDA (30) // ()
+#define MRBC_SSYM_Arduino (12) // Arduino
+#define MRBC_SSYM_puts (13) // puts
+#define MRBC_SSYM_ESC_01 (14) // +
+#define MRBC_SSYM_ESC_02 (15) // ===
+#define MRBC_SSYM_size (16) // size
+#define MRBC_SSYM_length (17) // length
+#define MRBC_SSYM_to_i (18) // to_i
+#define MRBC_SSYM_to_s (19) // to_s
+#define MRBC_SSYM_ESC_03 (20) // <<
+#define MRBC_SSYM_ESC_04 (21) // []
+#define MRBC_SSYM_ESC_05 (22) // []=
+#define MRBC_SSYM_chomp (23) // chomp
+#define MRBC_SSYM_ESC_06 (24) // chomp!
+#define MRBC_SSYM_dup (25) // dup
+#define MRBC_SSYM_index (26) // index
+#define MRBC_SSYM_ord (27) // ord
+#define MRBC_SSYM_to_sym (28) // to_sym
+#define MRBC_SSYM_intern (29) // intern
+#define MRBC_SSYM_sprintf (30) // sprintf
+#define MRBC_SSYM_ESC_LAMBDA (31) // ()
